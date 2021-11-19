08d31bfad690b4d627408be3b2e0539c6b4957b8
b1ruk <aptem122014@gmail.com>
2021-11-20 01:52:39 +0300

    Initial commit

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -7,7 +7,10 @@ import string
 def generate(length):  # Функция, генерирующая случайные символы
     letters_and_digits = string.ascii_letters + string.digits
     rand_string = ''.join(random.sample(letters_and_digits, length))
-    print(length, "случайных символов:", rand_string)
+    if length == 1:
+        print(length, "случайный символ:", rand_string)
+    else:
+        print(length, "случайных символов:", rand_string)
 
 
 x = input("Введи количество символов, которое нужно сгенерировать (не больше 62): ")
