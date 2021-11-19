77d3984b3aa6bac1f2fcd05acfd4cb2a2b96e759
b1ruk <aptem122014@gmail.com>
2021-11-20 01:50:28 +0300

    Initial commit

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -3,6 +3,13 @@
 import random
 import string
 
+
+def generate(length):  # Функция, генерирующая случайные символы
+    letters_and_digits = string.ascii_letters + string.digits
+    rand_string = ''.join(random.sample(letters_and_digits, length))
+    print(length, "случайных символов:", rand_string)
+
+
 x = input("Введи количество символов, которое нужно сгенерировать (не больше 62): ")
 
 while type(x) != int:  # Обработка исключений
@@ -14,12 +21,5 @@ while type(x) != int:  # Обработка исключений
     if type(x) == int and 0 >= x >= 63:
         print("Неправильный ввод!")
         x = input("Введи количество символов, которое нужно сгенерировать (не больше 62): ")
-
-
-def generate(length):  # Функция, генерирующая случайные символы
-    letters_and_digits = string.ascii_letters + string.digits
-    rand_string = ''.join(random.sample(letters_and_digits, length))
-    print(length, "случайных символов:", rand_string)
-
 
 generate(x)
